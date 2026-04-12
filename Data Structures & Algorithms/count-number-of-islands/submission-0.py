67bb03f5031e1466e278f32371ec1bc88d8e395f
Lucy Xu <llx2@cornell.edu>
2026-04-12 10:52:59 -0400

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        seen = [[False for col in grid[0]] for row in grid]
+        num = 0
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if seen[i][j] is False:
+                    seen[i][j] = True
+                    to_traverse = []
+                    if grid[i][j] == '1':
+                        to_traverse.append((i,j))
+                        while len(to_traverse) > 0:
+                            (visiting_i, visiting_j) = to_traverse.pop(0)
+                            seen[visiting_i][visiting_j] = True
+                            top = (visiting_i-1,visiting_j) if visiting_i != 0 else None
+                            down = (visiting_i+1,visiting_j) if visiting_i!= len(grid)-1 else None
+                            left = (visiting_i,visiting_j-1) if visiting_j != 0 else None
+                            right = (visiting_i,visiting_j+1) if visiting_j != len(grid[0])-1 else None
+                            
+                            for next_visit in [top, down, left, right]:
+                                # print(next_visit)
+                                if next_visit is not None:
+                                    (x,y) = next_visit
+                                    if not seen[x][y] and grid[x][y] == '1':
+                                        to_traverse.append((x,y))
+                        #     print('to_traverse', to_traverse)
+                        # print(i,j)
+                        num+=1
+        return num
+        
