e4d681bbd19bddd8339276dad78f4342cd3d2a6a
Lucy Xu <llx2@cornell.edu>
2026-04-12 21:21:33 -0400

    Add: rotting-fruit - submission-3

---
+++ Data Structures & Algorithms/rotting-fruit/submission-3.py
@@ -0,0 +1,37 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        to_traverse = deque()
+        visited = set()
+        directions = [(-1,0),(1,0),(0,1),(0,-1)]
+        num_fruits = 0
+        minutesPassed = 0
+        rotten_fruits = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 2 or grid[i][j] == 1:
+                    num_fruits+=1
+                if grid[i][j] == 2:
+                    to_traverse.append((i,j,0))
+        
+        while len(to_traverse) > 0:
+            (i,j,minute) = to_traverse.popleft()
+            minutesPassed = max(minutesPassed, minute)
+            rotten_fruits += 1
+            for (x,y) in directions:
+                new_i, new_j = i+x,j+y
+                if 0 <= new_i < len(grid):
+                    if 0 <= new_j < len(grid[0]):
+                        if (new_i, new_j) not in visited and grid[new_i][new_j] == 1:
+                            to_traverse.append((new_i,new_j,minute+1))
+                            visited.add((new_i,new_j))
+        
+        if num_fruits == 0:
+            return 0
+        elif rotten_fruits < num_fruits:
+            return -1
+        else:
+            return minutesPassed
+                
+                
+
+
